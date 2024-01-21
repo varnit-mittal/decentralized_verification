25c254542f3615296e959a8fec070517b0b538c2
varnit-mittal <Varnit.Mittal@iiitb.ac.in>
2024-01-21 19:16:00 +0530

    services added

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,6 +1,52 @@
+import React from "react";
+import { BsShieldFillCheck } from "react-icons/bs";
+import { TfiAgenda } from "react-icons/tfi";
+import { MdOutlineDomainVerification } from "react-icons/md";
+
+const ServiceCard=({color,title,icon,subtitle})=>(
+    <div className="flex flex-row justify-start items-start white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+    <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
+      {icon}
+    </div>
+    <div className="ml-5 flex flex-col flex-1">
+      <h3 className="mt-2 text-white text-lg">{title}</h3>
+      <p className="mt-1 text-white text-sm md:w-9/12">
+        {subtitle}
+      </p>
+    </div>
+  </div>
+)
+
 const Services=()=>{
     return(
-        <h1>Services</h1>
+        <div className="flex w-full justify-center items-center gradient-bg-services">
+            <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+                <div className="flex-1 flex flex-col justify-start items-start">
+                    <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">What we <br /> do?</h1>
+                </div>
+            </div>
+            <div className="flex-1 flex flex-col justify-start items-center">
+                <ServiceCard
+                    color="bg-[#2952E3]"
+                    title="Security Guranteed"
+                    icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+                    subtitle="Your documents are always safe with us on the eth blockchain"
+                />
+                <ServiceCard
+                    color="bg-[#8945F8]"
+                    title="Apply for Documents"
+                    icon={<TfiAgenda fontSize={21} className="text-white" />}
+                    subtitle="Apply for your personal documents from anywhere in the world"
+                />
+
+                <ServiceCard
+                    color="bg-[#F84550]"
+                    title="Verify any Document"
+                    icon={<MdOutlineDomainVerification fontSize={21} className="text-white" />}
+                    subtitle="We provide you with a unique feature of verifying any document"
+                />
+            </div>
+        </div>
     )
 }
 
