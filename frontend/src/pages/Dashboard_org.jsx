0e326965ee9cec5d706581cb578c55d313908e73
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-01-21 19:03:05 +0530

    org dashboard

--- frontend/src/pages/Dashboard_org.jsx
+++ frontend/src/pages/Dashboard_org.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
-
-const DashboardOrg = ({ orgName, numberOfPeople, issuedIds }) => {
+import { Navbar, Welcome, Footer, Services, Transactions } from "../components"
+const DashboardOrg = () => {
   return (
-    <div className="flex flex-col items-center mt-8">
-      <h2 className="text-3xl mb-4">
-        Welcome, {orgName}!
+    <div className=" relative gradient-bg-welcome w-full h-screen flex flex-col items-center ">
+    <div className='absolute top-0 w-full'>
+<Navbar/>
+    </div>
+  <h2 className="text-5xl mb-4 font-bold absolute top-60 left-40 text-white">
+        Welcome, IIITB! 🙏
       </h2>
-      <div className="text-left mb-4">
+      <div className="text-left text-3xl text-white font-semibold mb-4 absolute top-[370px]">
         <p>
-          Number of People in the Organization: {numberOfPeople}
+          Number of People in the Organization: 1245
         </p>
       </div>
-      <div className="text-left">
-        <h3 className="text-xl mb-2">Issued IDs:</h3>
-        <ul className="list-disc pl-4">
-          {issuedIds.map((id) => (
-            <li key={id}>
-              {id.name} - {id.id}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <h3 className="text-4xl mb-2  font-bold absolute top-[470px] left-[800px]     text-white">Issued Documents:</h3>
+      <div className="text-left text-2xl absolute bottom-[80px] flex justify-between text-black ">
+  <div className="card ">Document 1</div>
+  <div className="card">Document 2</div>
+  <div className="card">Document 3</div>
+</div>
     </div>
   );
 };
