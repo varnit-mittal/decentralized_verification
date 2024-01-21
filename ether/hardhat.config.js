c9378dd70ee360454362e8cc628eb083ca936d73
ap5967ap <aditya.priyadarshi@iiitb.ac.in>
2024-01-21 15:21:56 +0530

    abi

--- ether/hardhat.config.js
+++ ether/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia:{
+      url:process.env.RPC,
+      accounts:[process.env.PRIVATE_KEY]
+    }
+  },
+  paths:{
+    sources:"./contracts"
+  }
 };
+
+console.log(process.env.RPC)
+
+// Organization deployed to: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+// User deployed to: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+// Verification deployed to: 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
--- ether/scripts/deploy.js
+++ ether/scripts/deploy.js
@@ -1,28 +1,20 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const org=await hre.ethers.getContractFactory("organization");
+  const Org=await org.deploy();
+  await Org.deployed();
+  // console.log("Organization deployed to:",Org.address);
+  const user=await hre.ethers.getContractFactory("user");
+  const User=await user.deploy();
+  await User.deployed();
+  // console.log("User deployed to:",User.address);
+  const verification=await hre.ethers.getContractFactory("verification");
+  const Verification=await verification.deploy();
+  await Verification.deployed();
+  // console.log("Verification deployed to:",Verification.address);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
